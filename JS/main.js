97a16154fcc43d9c5b3e66bcd816e00a74512031
Programozas Karrier <info@programozaskarrier.hu>
2021-12-14 19:24:32 +0100

    async function await

--- JS/main.js
+++ JS/main.js
@@ -1,108 +1,80 @@
-/*
-    Login url: https://reqres.in/api/login
-    Body:
-    {
-      email: "eve.holt@reqres.in",
-      password: "ok"
-    }
-
-    Users url: https://reqres.in/api/users
-*/
-
 var state = {
     users: [],
-    isLoggedIn: false,
-    isLoginPending: false
-};
-
-window.onload = renderLoginComponent;
-
-function renderLoginComponent() {
-    if (state.isLoggedIn) {
-        document.getElementById("login-component").innerHTML = "";
-        return;
-    }
-
-    document.getElementById("login-component").innerHTML = `
-        <div class="card p-3">    
-          <h1>Bejelentkezés</h1>   
-          <form id="login" class="p-3">
-            <label class="w-100">
-              Email:
-              <input type="text" name="email" class="form-control" ${state.isLoginPending ? "disabled" : ""
-        }  />
-            </label>
-            <label class="w-100">
-              Jelszó:
-              <input type="password" name="password" class="form-control" ${state.isLoginPending ? "disabled" : ""
-        } />
-            </label>
-            <button type="submit" class="btn btn-primary" ${state.isLoginPending ? "disabled" : ""
-        }>Küldés</button>
-            <div id="message" class="float-right mt-2">
-              ${state.isLoginPending ? "Bejelentkezés folyamatban..." : ""}
-            </div>
-          </form>
-        </div>
-    `;
-
-    document.getElementById("login").onsubmit = function (event) {
-        event.preventDefault();
-        var email = event.target.elements.email.value;
-        var password = event.target.elements.password.value;
-        var body = JSON.stringify({
-            email: email,
-            password: password
-        });
-        state.isLoginPending = true;
-        renderLoginComponent();
-
-
-        fetch("https://reqres.in/api/login", {
-            method: "POST",
-            body: body,
-            headers: {
-                "Content-type": "application/json"
-            }
-        })
-            .then(function (vadtoken) {
-                if (!vadtoken.ok) {
-                    return Promise.reject("Bejelentkezés sikertelen");
-                }
-                return vadtoken.json();
-            })
-            .then(function (jotoken) {
-                state.isLoggedIn = true;
-                state.isLoginPending = false;
-                renderLoginComponent();
-                return fetch("https://reqres.in/api/users");
-            })
-            .then(function (vaduser) {
-                if (!vaduser.ok) {
-                    return Promise.reject("users error");
-                }
-                return vaduser.json();
-            })
-            // action
-            .then(function (jouser) {
-                // state change
-                state.users = jouser.data;
-                // render
-                renderUsers();
-            })
-            .catch(function (error) {
-                state.isLoginPending = false;
-                renderLoginComponent();
-                alert(error);
-            });
-
-    };
+    loggedIn: false
 }
 
-function renderUsers() {
-    var userHTML = '';
+
+document.getElementById('login-component').innerHTML = `
+ <h1>Bejelentkezés</h1>   
+            <form id="login" class="p-3">
+              <label class="w-100">
+                Email:
+                <input type="text" name="email" class="form-control"/>
+              </label>
+              <label class="w-100">
+                Jelszó:
+                <input type="password" name="password" class="form-control"/>
+              </label>
+              <button type="submit" class="btn btn-primary">Küldés</button>
+              <div id="message" class="float-right mt-2">
+              </div>
+            </form>
+
+`
+
+
+
+function loggedin() {
+    if (state.loggedIn) {
+        document.getElementById('login-component').innerHTML = ''
+        return
+    }
+}
+
+
+document.getElementById('login').onsubmit = function (event) {
+    event.preventDefault()
+    var email = event.target.elements.email.value;
+    var password = event.target.elements.password.value;
+    var body = JSON.stringify({
+        email: email,
+        password: password
+    }) 
+    fetchUser(body) 
+}
+
+async function fetchUser(body) {
+    var vadresp = await fetch('https://reqres.in/api/login', {
+        method: 'POST',
+        body: body,
+        headers: { 'content-type': 'application/json' }
+    });
+    if (!vadresp.ok) {
+        alert('Hibás jelszó')
+        return
+    }
+    var joresptoken = await vadresp.json();
+    state.loggedIn = true
+    loggedin()
+    
+    var vaduser = await fetch('https://reqres.in/api/users');
+    if (!vaduser.ok) {
+        alert('Hibás user')
+        return
+    }
+    var jouser = await vaduser.json()
+    state.users = jouser.data
+    renderuser()
+}
+
+
+function renderuser() {
+    var userHTML = ''
     for (var st of state.users) {
-        userHTML += `<li>${st.first_name} ${st.last_name}</li>`
+        userHTML += `<li>${st.first_name}${st.last_name}</li>`
     }
-    document.getElementById("user-list-container").innerHTML = '<ul>' + userHTML + '</ul>'
+    document.getElementById('post-list-container').innerHTML = '<ol>' + userHTML + '</ol>';
 }
+
+
+window.onload = loggedin()
